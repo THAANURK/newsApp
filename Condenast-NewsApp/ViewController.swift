d71feaed08fafec930eb469e2d4f042d6a398752
ShreeThaanu <shree96thaanu@gmail.com>
2022-01-20 00:05:52 +0530

    Initial Commit

---
+++ Condenast-NewsApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Condenast-NewsApp
+//
+//  Created by shree thaanu on 19/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
